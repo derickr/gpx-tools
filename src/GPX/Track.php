4389cd2e49e0aa7eefc40e47f8d9b6a91ff326b1
Derick Rethans <github@derickrethans.nl>
2022-08-12 12:27:38 +0100

    Added 'truncatePrecision' function

--- src/GPX/Track.php
+++ src/GPX/Track.php
@@ -35,5 +35,33 @@ class Track
 	{
 		$this->track = array_reverse( $this->track );
 	}
+
+	function truncatePrecision( int $decimals )
+	{
+		$tmpTrack = array_map(
+			function($pair) use ( $decimals ) {
+				$factor = pow( 10, $decimals );
+
+				$pair[0] = floor($pair[0] * $factor) / $factor;
+				$pair[1] = floor($pair[1] * $factor) / $factor;
+
+				return $pair;
+			},
+			$this->track
+		);
+
+		$this->track = [];
+
+		$currentPair = [0, 0];
+
+		foreach ( $tmpTrack as $pair )
+		{
+			if ( $currentPair[0] != $pair[0] || $currentPair[1] != $pair[1] )
+			{
+				$this->track[] = $pair;
+				$currentPair = $pair;
+			}
+		}
+	}
 }
 ?>
